54f726aecd2d16e33e7c8c508cd6c64af27f1fda
aquadzn <williamjcqs8@gmail.com>
2020-05-12 10:53:08 +0200

    removed stuff

--- front/vue.config.js
+++ front/vue.config.js
@@ -1,7 +1,7 @@
 const path = require("path");
 
 module.exports = {
-    outputDir: path.resolve(__dirname, "../docs/dist"),
+    outputDir: path.resolve(__dirname, "../docs/"),
     publicPath: process.env.NODE_ENV === 'production'
       ? '/website/'
       : '/',
