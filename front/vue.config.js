b4b6d6aa90e9b0b93d8a78f7dfd4bec74e72624a
aquadzn <williamjcqs8@gmail.com>
2020-05-12 11:37:37 +0200

    fuck

--- front/vue.config.js
+++ front/vue.config.js
@@ -1,8 +1,8 @@
 const path = require("path");
 
 module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/background-removal/'
+    : '/',
     outputDir: path.resolve(__dirname, "../docs/"),
-    publicPath: process.env.NODE_ENV === 'production'
-      ? '/background-removal/'
-      : '/',
-  };
+}
