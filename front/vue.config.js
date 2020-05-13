095c24c4b610a5b37470e863a600e825a029f360
aquadzn <williamjcqs8@gmail.com>
2020-05-13 21:11:47 +0200

    Removed front stuff. Will add better version

--- front/vue.config.js
+++
@@ -1,8 +0,0 @@
-const path = require("path");
-
-module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/background-removal/'
-    : '/',
-    outputDir: path.resolve(__dirname, "../docs/"),
-}
